86dc513a959c8e8faf8a7f706bcc4b3909796d0f
jonsan1969 <111865364+jonsan1969@users.noreply.github.com>
2026-08-17 08:25:53 +0200

    Extend branch-analysis slice through CreateBJLSequence

--- tests/prepare_portable_decompiler_branch_slice.ps1
+++ tests/prepare_portable_decompiler_branch_slice.ps1
@@ -1,6 +1,6 @@
 $src = Get-Content -Raw -LiteralPath "$PSScriptRoot\..\Decompiler.cpp"
 $start = $src.IndexOf('bool __fastcall TDecompileEnv::GetBJLRange(')
-$end = $src.IndexOf('void __fastcall TDecompileEnv::CreateBJLSequence(', $start)
+$end = $src.IndexOf('void __fastcall TDecompileEnv::UpdateBJLList()', $start)
 if ($start -lt 0 -or $end -lt 0) { throw 'Decompiler branch slice markers not found' }
 $body = $src.Substring($start, $end - $start)
 $prefix = @'
@@ -13,6 +13,8 @@ extern Byte *Code;
 int __fastcall Adr2Pos(DWord adr);
 bool __fastcall IsFlagSet(DWord flag, int pos);
 int __fastcall BranchGetPrevInstructionType(DWord fromAdr, DWord *jmpAdr, PLoopInfo loopInfo);
+String __fastcall GetDirectCondition(char c);
+String __fastcall GetInvertCondition(char c);
 
 '@
 New-Item -ItemType Directory -Force -Path "$PSScriptRoot\generated" | Out-Null
